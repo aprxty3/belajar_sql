b2095b7dec65c966ac93bf1a28cac8194a0594a1
aji prasetyo <ajiprxty3@gmail.com>
2025-04-23 11:04:47 +0700

    join

--- wishlist.sql
+++ wishlist.sql
@@ -40,3 +40,42 @@ where id = 'P009';
 
 insert into wishlist (id_product, description)
 values ('P009', 'suka');
+
+select *
+from wishlist
+         join products on (wishlist.id_product = products.id);
+
+select w.id          as wishlist_id,
+       w.description as wishlist_description,
+       p.id          as product_id,
+       p.name        as product_name
+from wishlist as w
+         join products as p on (w.id_product = p.id);
+
+describe customer;
+describe wishlist;
+show create table wishlist;
+
+select *
+from wishlist;
+
+alter table wishlist
+    add column id_customer int;
+
+alter table wishlist
+    drop column id_customer;
+
+alter table wishlist
+    add constraint fk_wishlist_customer foreign key (id_customer) references customer (id);
+
+alter table wishlist
+    drop constraint fk_wishlist_customer;
+
+update wishlist
+set id_customer = 1
+where id = 2;
+
+select c.email, p.id, p.name, w.description
+from wishlist as w
+         join products as p on (p.id = w.id_product)
+         join customer as c on (c.id = w.id_customer);
